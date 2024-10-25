92ad31216d147ec8c838b8774afb88b84eea4a38
ulieth <uliana.eth@proton.me>
2024-10-25 17:34:48 +0300

    extracting the argument parser

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,8 +8,14 @@ fn main() {
     // The first argument's beign the program's name:
     // [src/main.rs:7:5] args = [
     //  "target/debug/minigrep", ]
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
+
+    fn parse_config(args: &[String]) -> (&str, &str) {
+      let query = &args[1];
+      let file_path = &args[2];
+
+      (query, file_path)
+    }
 
     println!("Searching for {query}");
     println!("In file {file_path}");
