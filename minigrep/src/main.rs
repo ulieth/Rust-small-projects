e24cc9467ba10091b39350a5d35d31ff9e8c726d
ulieth <uliana.eth@proton.me>
2024-10-25 18:13:15 +0300

    error handling added

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 
 
@@ -8,18 +9,24 @@ fn main() {
     // The first argument's beign the program's name:
     // [src/main.rs:7:5] args = [
     //  "target/debug/minigrep", ]
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     struct Config {
       query: String,
       file_path: String,
     }
     impl Config {
-        fn new(args: &[String]) -> Config {
+        fn build(args: &[String]) -> Result<Config, &'static str> {
+          if args.len() < 3 {
+            return Err("not enough arguments");
+          }
           let query = args[1].clone();
           let file_path = args[2].clone();
 
-          Config { query, file_path }
+          Ok(Config { query, file_path })
       }
 
     }
