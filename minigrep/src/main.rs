c55b4b7c928936aa7ce39c75ea00e9df486e8e2d
ulieth <uliana.eth@proton.me>
2024-10-25 17:52:52 +0300

    grouping config values

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,19 +8,24 @@ fn main() {
     // The first argument's beign the program's name:
     // [src/main.rs:7:5] args = [
     //  "target/debug/minigrep", ]
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    fn parse_config(args: &[String]) -> (&str, &str) {
-      let query = &args[1];
-      let file_path = &args[2];
-
-      (query, file_path)
+    struct Config {
+      query: String,
+      file_path: String,
     }
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    fn parse_config(args: &[String]) -> Config {
+      let query = args[1].clone();
+      let file_path = args[2].clone();
+
+      Config { query, file_path }
+    }
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
     // adding file-reading capabilities
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
 
