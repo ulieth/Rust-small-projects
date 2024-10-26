43b8c3b19ca5349e7b5d8b44b41c5046b21fce1b
ulieth <uliana.eth@proton.me>
2024-10-26 13:55:10 +0300

    adding a case sensitivity test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -31,24 +31,49 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 mod tests {
     use super::*;
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+      let query = "rUsT";
+      let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+      assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents))
+
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
+    let mut results = Vec::new();
 
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
+    for line in contents.lines() {
+        if line.contains(query) {
+        results.push(line);
+        }
     }
-  }
-  results
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+      if line.to_lowercase().contains(&query) {
+        results.push(line)
+      }
+    }
+    results
 }
