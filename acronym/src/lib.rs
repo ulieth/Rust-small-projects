ee7e6c56fa962df55de101362a3442d8ea21d0fc
ulieth <uliana.eth@proton.me>
2024-11-01 23:25:26 +0300

    Split the phrase using non-alphanumeric characters as delimiters

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,13 +1,10 @@
 pub fn abbreviate(phrase: &str) -> String {
-  // Split the phrase into words and collect the first letter of each word
+  // Split the phrase using non-alphanumeric characters as delimiters
   let acronym: String = phrase
-      .split_whitespace() // Split by whitespace
-      .filter_map(|word| {
-          // Use filter_map to ignore empty words and collect the first letter
-          word.chars().next()
-      })
-      .map(|c| c.to_ascii_uppercase()) // Convert to uppercase
-      .collect(); // Collect into a String
+  .split(|c: char| !c.is_alphanumeric()) // Split on non-alphanumeric characters
+  .filter_map(|word| word.chars().next()) // Collect the first letter of each word
+  .map(|c| c.to_ascii_uppercase()) // Convert to uppercase
+  .collect(); // Collect into a String
 
   acronym
 }
@@ -21,6 +18,14 @@ mod tests {
         let input = "Portable Network Graphics";
         let output = abbreviate(input);
         let expected = "PNG";
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn punctuation() {
+        let input = "First In, First Out";
+        let output = abbreviate(input);
+        let expected = "FIFO";
         assert_eq!(output, expected);
     }
 }
